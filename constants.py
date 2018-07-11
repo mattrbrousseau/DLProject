02c6ae786ad1ef8f51b5e8a8f7d3ea38ca872136
Miria Rafante <m063244@yahoo.com.br>
2018-07-11 15:23:26 -0300

    adding file for constants as a package

---
+++ constants.py
@@ -0,0 +1,12 @@
+
+
+labels = ["Tearing","Bus","Shatter","Gunshot, gunfire","Fireworks","Writing",\
+		"Computer keyboard","Scissors","Microwave oven","Keys jangling",\
+		"Drawer open or close","Squeak","Knock","Telephone","Saxophone",\
+		"Oboe","Flute","Clarinet","Acoustic guitar","Tambourine","Glockenspiel",\
+		"Gong","Snare drum","Bass drum","Hi-hat","Electric piano","Harmonica",\
+		"Trumpet","Violin, fiddle","Double bass","Cello","Chime","Cough",\
+		"Laughter","Applause","Finger snapping","Fart","Burping, eructation",\
+		"Cowbell","Bark","Meow"]
+
+		
